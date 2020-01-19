24ff4993d4110b0248607ca292ff7cd620293c8e
Ananya Sharma <ananyasharma@AnanyaMacBookPro.local>
2020-01-19 14:32:48 +0530

    initial commit

---
+++ src/main/java/com/example/Merchant/MicroService/MerchantMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Merchant.MicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Merchant/MicroService/MerchantMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Merchant.MicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
