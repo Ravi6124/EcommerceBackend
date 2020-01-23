83b77b0efc351990dc87f124182b23bdb66df6e9
Raghav Agarwal <raghavagarwal@Raghavs-MacBook-Pro.local>
2020-01-23 16:02:12 +0530

    cartintegrated

--- src/main/java/com/example/ZuulApiGateway/ZuulApiGatewayApplication.java
+++ src/main/java/com/example/ZuulApiGateway/ZuulApiGatewayApplication.java
@@ -3,13 +3,13 @@ package com.example.ZuulApiGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulApiGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApiGatewayApplication.class, args);
-	}
 
-}	
+	}
+}
