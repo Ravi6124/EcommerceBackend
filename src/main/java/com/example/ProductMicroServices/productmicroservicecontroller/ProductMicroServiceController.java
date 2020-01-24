7259305ae55f6ca8d4032ebf1ce10e05290ccee2
Raghav Agarwal <raghavagarwal@Raghavs-MacBook-Pro.local>
2020-01-24 12:50:59 +0530

    cross origin

--- src/main/java/com/example/ProductMicroServices/productmicroservicecontroller/ProductMicroServiceController.java
+++ src/main/java/com/example/ProductMicroServices/productmicroservicecontroller/ProductMicroServiceController.java
@@ -22,6 +22,7 @@ import java.util.function.Function;
 
 @RestController
 @RequestMapping("product")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProductMicroServiceController {
 
     @Autowired
