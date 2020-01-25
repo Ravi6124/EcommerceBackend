ae8b6328e9998858f693b1ca86029bdd10374c45
Raghav Agarwal <raghavagarwal@Raghavs-MacBook-Pro.local>
2020-01-25 09:56:00 +0530

    eureka client on zuul

--- src/main/java/com/example/ZuulApiGateway/ZuulApiGatewayApplication.java
+++ src/main/java/com/example/ZuulApiGateway/ZuulApiGatewayApplication.java
@@ -2,11 +2,13 @@ package com.example.ZuulApiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableEurekaClient
 public class ZuulApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApiGatewayApplication.class, args);
